7a1adc970900ae0bb7c52acf8caca307e0d95d7d
gianfi12 <10587213@polimi.it>
2021-03-31 22:42:52 +0200

    Update report with interfaces

--- render_object/draw_object/draw_object.h
+++ render_object/draw_object/draw_object.h
@@ -4,13 +4,19 @@
 #include <string>
 using namespace std;
 
+/* Is an abstraction that is used in order to have the directive to know what should be printed on screen */
 class DrawObject{
     public:
+        /* Is the constructor of this object */
         DrawObject(string object_string,string color):object_string(object_string),color(color){};
+        /* Returns the string associated to this object that should be printed */
         string getObjectString() { return object_string; };
+        /* Is the color of the string that has to be printed */
         string getObjectColor() { return color; };
     private:
+        /* Is the string associate to this DrawObject */
         string object_string;
+        /* Is the color associate to this DrawObject */
         string color;
 };
 
